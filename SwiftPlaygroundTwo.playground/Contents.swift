28e5634892044ea9d4a83ccdb4c88ef3285e2b08
Kaylene <carnavos@hawaii.edu>
2022-09-21 20:28:35 -1000

    Part 2

--- SwiftPlaygroundTwo.playground/Contents.swift
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
 var greeting = "Hello, playground"
+var goodGrade = "I've been doing all my work"
+if goodGrade == "I've been doing all my work"
+{
+    print("I should be getting an A at this point")
+}
+if goodGrade == "I'm not doing very well"
+    
