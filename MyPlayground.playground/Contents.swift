25ed6fd75cac26f30efa071421c0eb4b8b4b0ead
Kaylene <carnavos@hawaii.edu>
2022-09-29 11:47:40 -1000

    Project One!

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+var greeting = "Hello, playground"
+//Variable Coding Challenge #1
+var firstString : String
+var secondString : String
+firstString = "Aloha"
+secondString = "Hola"
+var thirdString : String
+thirdString = "Hello"
+firstString + secondString == thirdString
+print(thirdString)
+
+//Variable Coding Challenge #2
+var farenheit : Float = 50
+//(x°F − 32) × 5/9 = y°C
+var celsius : Float = 10
+print("Today is 10 degress celsius")
+
+//Boolean Varaibles Challenge #1
+var numberOne : Int
+var numberTwo : Int
+numberOne = 5
+numberTwo = 5
+var numberThree = numberOne == numberTwo
+
+//If Coding Challenge #1
+var numeroUno : String
+numeroUno = "Pixie Dust"
+var numeroDos = "Angel Light"
+if numeroUno > numeroDos
+{
+    print("Good job!")
+}
+else
+{
+    print ("Try again")
+}
